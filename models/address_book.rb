7f2c3eda386476b0cfef2acb94508092925a86e7
Sam Smith <smithrunner@live.com>
2017-08-15 19:15:05 +0000

    assignment 7 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -62,4 +62,16 @@ class AddressBook
       
       return nil      
     end
+    
+    def iterative_search(name)
+      index = 0
+      entries.each do |entry|
+        if entry.name == name
+          return entries[index]
+        else
+          index+=1
+        end
+      end
+      return nil
+    end
 end
